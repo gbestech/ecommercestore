b2c79011ea30f008994e489d61371ac1483573dd
G-bestech <mustapha371@gmail.com>
2024-01-03 17:27:35 -0800

    added bage in a header

--- myecommerce/src/components/Header.jsx
+++ myecommerce/src/components/Header.jsx
@@ -1,12 +1,13 @@
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Badge, Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import { LinkContainer } from "react-router-bootstrap";
 import { useSelector } from "react-redux";
 
 const Header = () => {
-  // const  cartItems  = useSelector((state) => state.cart);
+  const {cartItems} = useSelector((state) => state.cart);
 
-  // console.log(cartItems);
+  console.log(cartItems);
+  console.log(cartItems.length)
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
@@ -20,6 +21,11 @@ const Header = () => {
               <LinkContainer to="/cart">
                 <Nav.Link>
                   <FaShoppingCart /> Cart
+                  {cartItems.length > 0 && (
+                    <Badge pill bg='success' style={{ marginLeft: '5px' }}>
+                      {cartItems.reduce((a, c) => a + c.qty, 0)}
+                    </Badge>
+                  )}
                 </Nav.Link>
               </LinkContainer>
               <LinkContainer to="/login">
@@ -36,3 +42,6 @@ const Header = () => {
 };
 
 export default Header;
+
+
+
